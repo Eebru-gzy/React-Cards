1229e0e868c7bba717a0bd946ef92a5819e8e5b2
Ibrahim <Alaoopeyemi101@gmail.com>
2020-03-25 17:14:49 +0100

    all commits

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Ui from '../src/components/ui'
+
+
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+     data: [],
+      images: [
+       "https://randomuser.me/api/portraits/women/48.jpg",
+       "https://randomuser.me/api/portraits/men/94.jpg",
+       "https://randomuser.me/api/portraits/men/54.jpg",
+       "https://randomuser.me/api/portraits/men/4.jpg",
+       "https://source.unsplash.com/random/350x200",
+       "https://source.unsplash.com/random/350x200",
+       "https://source.unsplash.com/random/350x200",
+       "https://source.unsplash.com/random/350x200",
+       "https://source.unsplash.com/random/350x200",
+       "https://source.unsplash.com/random/350x200"
+     ]
+    };
+  }
+
+  async componentDidMount() {
+    const api = fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await api;
+    const jason = await data.json();
+      
+    this.setState({
+    data: [...this.state.data, ...jason]
+    });
+  };   
+
+      render() { 
+        return ( 
+          <div>
+            <Ui data={this.state.data} images={this.state.images} />
+          </div>
+          
+     );
+  }
+}
+ 
+export default App;
---
+++ src/components/ui.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+// import Image from './images';
+
+
+class Ui extends Component {
+
+  render() { 
+
+    const { data, images } = this.props;
+
+    // const imgs = images.forEach((item, i) => {
+
+    //   return <img src={item} alt="" key={i} />
+      
+    // })
+
+    for (let i = 0; i < images.length; i++) {
+      <img src={i} alt="" />
+    }
+
+    
+    const values = data.map((items, i) => {
+      return (
+        <div className="card m-3" key={i}>
+          <div className="card-image">
+
+            
+
+          </div>
+        <div className="card-body">
+         <h4 className="card-title">Name: {items.name}</h4>
+         <p className="card-text"> UserName: {items.username}</p>
+         <p className="card-text"> Email: {items.email}</p>
+         <p className="card-text">
+          Address: {`${items.address.street}, ${items.address.city}`}
+         </p>
+         <p className="card-text"> Phone Number: {items.phone}</p>
+        </div>
+       </div>
+      );
+    });
+    return <div className="card-columns container">{values}</div>
+
+  }
+}
+ 
+export default Ui;
