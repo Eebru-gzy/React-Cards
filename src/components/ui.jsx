4af38790cd68419011042b0207d40acc9a618277
Ibrahim <Alaoopeyemi101@gmail.com>
2020-03-27 20:50:34 +0100

    fix image loop

--- src/components/ui.jsx
+++ src/components/ui.jsx
@@ -1,41 +1,31 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 // import Image from './images';
 
-
 class Ui extends Component {
+ render() {
+  const { data, images } = this.props;
 
-  render() { 
-
-    const { data, images } = this.props;
-
-    const imgs = images.map((item, i) => {
-      return <img src={item} alt="" key={i} />
-    })
-    
-    const values = data.map((items, i) => {
-      return (
-        <div className="card m-3" key={i}>
-          <div className="card-image">
-            
-            {imgs}
-
-          </div>
-        <div className="card-body">
-         <h4 className="card-title">Name: {items.name}</h4>
-         <p className="card-text"> UserName: {items.username}</p>
-         <p className="card-text"> Email: {items.email}</p>
-         <p className="card-text">
-          Address: {`${items.address.street}, ${items.address.city}`}
-         </p>
-         <p className="card-text"> Phone Number: {items.phone}</p>
-        </div>
-       </div>
-      );
-    });
-    return <div className="card-columns container">{values}</div>
-
-  }
+  const values = data.map((items, i) => {
+   return (
+    <div className="card m-3" key={i}>
+     <div className="card-image">
+      <img src={images[i]} alt="" key={i} />
+     </div>
+     <div className="card-body">
+      <h4 className="card-title">Name: {items.name}</h4>
+      <p className="card-text"> UserName: {items.username}</p>
+      <p className="card-text"> Email: {items.email}</p>
+      <p className="card-text">
+       Address: {`${items.address.street}, ${items.address.city}`}
+      </p>
+      <p className="card-text"> Phone Number: {items.phone}</p>
+     </div>
+    </div>
+   );
+  });
+  return <div className="card-columns container">{values}</div>;
+ }
 }
- 
+
 export default Ui;
